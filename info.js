bd97cfd5f3c2d372faaf3e4793ab07a2e1b7e822
ebadgio <eb27@rice.edu>
2018-03-20 01:08:27 -0500

    for heroku

--- info.js
+++ info.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:3000/';
-// const URL = 'https://evening-lowlands-50176.herokuapp.com/';
+// const URL = 'http://localhost:3000/';
+const URL = 'https://www.stackhacktutoring.com/';
 
 export default URL;
