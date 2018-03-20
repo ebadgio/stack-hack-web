f1e2bbd8d8039fcacaf49f1b7cdeb3396fa80282
ebadgio <eb27@rice.edu>
2018-03-20 14:47:25 -0500

    nodemailer

--- server/Routes.js
+++ server/Routes.js
@@ -3,36 +3,48 @@ const express = require('express');
 const router = express.Router();
 // const contact = require('./resources/contact');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'stackhack.team@gmail.com',
+        pass: 'stackhack!'
+    }
+});
+
 
 router.post('/add/contact', (req, res) => {
-    try {
-        // console.log('req', req.body);
-        // console.log('contact', contact);
-        // //console.log('parsed', JSON.parse(contact));
-        // const copy = Array.from(contact);
-        // copy.push(req.body);
-        // console.log('added', copy);
-        // let send = JSON.stringify(copy);
-        // fs.writeFileSync('contact.json', send);
-        fs.readFile('contact.json', (err, data) => {
-            if (err) throw err;
-            const parsed = JSON.parse(data);
-            //const copy = Array.from(data);
-            parsed.push(req.body);
-            console.log('parsed added', parsed);
-            const json = JSON.stringify(parsed, null, 2);
-            fs.writeFile('contact.json', json, (err) => {
-                if (err) throw err;
-                console.log('Data written to file');
-                res.json({success: true})
-            })
+
+        // fs.readFile('contact.json', (err, data) => {
+        //     if (err) throw err;
+        //     const parsed = JSON.parse(data);
+        //     //const copy = Array.from(data);
+        //     parsed.push(req.body);
+        //     console.log('parsed added', parsed);
+        //     const json = JSON.stringify(parsed, null, 2);
+        //     fs.writeFile('contact.json', json, (err) => {
+        //         if (err) throw err;
+        //         console.log('Data written to file');
+        //         res.json({success: true})
+        //     })
+        // });
+        const mailOptions = {
+            from: 'stackhack.team@gmail.com',
+            to: 'stackhack.team@gmail.com',
+            subject: 'NEW CONTACT: ' + req.body.fname + ' ' + req.body.lname,
+            html: `<div><h1>First Name: ${req.body.fname}</h1><br/><h1>Last Name: ${req.body.lname}</h1><h1>Email: ${req.body.email}</h1><br/><p>${req.body.discussion}</p></div>`
+        };
+        transporter.sendMail(mailOptions,  (err, info) => {
+            if (err) {
+                console.log('err', err);
+                res.json({success: false, err: err});
+            }
+            else {
+                console.log('info', info);
+                res.json({success: true, info: info});
+            }
         });
-    } catch(error) {
-        console.log('err', error);
-        res.json({success: false});
-    }
-
-
 });
 
 module.exports = router;
